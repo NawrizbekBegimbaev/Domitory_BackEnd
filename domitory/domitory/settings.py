a2b08e74ad01e5ac652df74e25372fa7b650e12c
NawrizbekBegimbaev <nbegimbaev2006@gmail.com>
2024-11-23 02:56:54 +0500

    Добавлены изменения в settings.py и обновлён .gitignore

--- domitory/domitory/settings.py
+++ domitory/domitory/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY = 'django-insecure-4p138slz#f1jmhd^%a4l#s_onsdin_i&l%=&8pdeitm7n7i#8c
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['domitory.herokuapp.com']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
