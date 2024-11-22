a897d8d2170f99386d35f32c8d86fb83440bc73b
NawrizbekBegimbaev <nbegimbaev2006@gmail.com>
2024-11-23 04:20:18 +0500

    123

--- domitory/domitory/settings.py
+++ domitory/domitory/settings.py
@@ -78,12 +78,7 @@ WSGI_APPLICATION = 'domitory.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+DATABASES = {'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))}
 
 
 
